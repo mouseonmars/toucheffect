b4e46312044276fc01c9578b7693ac39d0d0e268
Michael Causer <michael.causer@se.com>
2021-01-05 11:59:57 -0700

    Initial commit

---
+++ TouchTrackingEffect/TouchTrackingEffect.Droid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TouchTrackingEffectDemos.Droid
+{
+    [Activity(Label = "TouchTrackingEffectDemos", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override bool DispatchTouchEvent(MotionEvent ev)
+        {
+            Console.WriteLine("DispatchTouchEvent");
+            return base.DispatchTouchEvent(ev);
+        }
+
+
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            Console.WriteLine("OnTouchEvent");
+            return base.OnTouchEvent(e);
+        }
+    }
+}
+
---
+++ TouchTrackingEffect/TouchTrackingEffect/HomePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+using Esri.ArcGISRuntime.Mapping;
+using TouchTracking;
+using Xamarin.Forms;
+
+namespace TouchTrackingEffectDemos
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+
+            MyMapView.Map = new Map(
+                BasemapType.Imagery, // use the Imagery basemap
+                42.175131,           // latitude
+                9.192313,            // longitude
+                8                    // level of detail (lower number = smaller scale))
+                );
+            BindingContext = this;
+        }
+
+
+
+        private void TouchEffect_BoxView(object sender, TouchActionEventArgs args)
+        {
+            DisplayAlert("touch", "box view", "close");
+        }
+
+        private void TouchEffect_MapView(object sender, TouchActionEventArgs args)
+        {
+            DisplayAlert("touch", "map view", "close");
+        }
+    }
+}
